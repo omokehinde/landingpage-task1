52d190973b7db4b33708955a8188ad96dd303a80
omokehinde <engr.omokehinde@gmail.com>
2022-12-24 12:31:43 +0100

    Navbar with bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,34 @@
-import { useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
 import '../styles/navbar.css';
 import dananz from "../images/dananz.png";
-import Home from "../pages/Home";
+
 
 function Navbar() {
-	const navRef = useRef();
-
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
-	};
 
 	return (
-		<header>
-            <a href="/">
-            <img src={dananz} />
-            </a>
-			<nav ref={navRef}>
-				<a href="/#">Home</a>
-				<a href="/#">About Us</a>
-				<a href="/#">Services</a>
-				<a id="b" href="/#">Our Teams</a>
-                <button className="btn-block">Contact US</button>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button id="hideBars" className="nav-btn" onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
+		<nav className="navbar navbar-expand-lg navbar-light bg-white">
+  <a className="navbar-brand" href="#"><img src={dananz} /></a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <a className="nav-link" href="#">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About Us</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link text-bold" href="#">Our Teams</a>
+      </li>
+    </ul>
+    <button className='btn-block'>Contact Us</button>
+  </div>
+</nav>
 	);
 }
 
